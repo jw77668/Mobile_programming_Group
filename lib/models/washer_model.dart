f119800f90cc0c9de69960d7d6d623e9e7cbf458
jeawo <jeawoo77668@gmail.com>
2025-12-03 00:38:31 +0900

    이름 수정

--- lib/models/washer_model.dart
+++ lib/models/washer_model.dart
@@ -34,37 +34,37 @@ class WasherModel {
     return [
       // SEW 1 설명서 공통
       WasherModel(
-        washerCode: 'Samsung 3G105A',
+        washerCode: 'Samsung_3G105A',
         washerName: '삼성 Bespoke AI 세탁기 25kg',
         manualPath: 'assets/data/Samsung_SEW_1.pdf',
         imagePath: 'assets/images/3G105A.png',
       ),
       WasherModel(
-        washerCode: 'Samsung 3G106S',
+        washerCode: 'Samsung_3G106S',
         washerName: '삼성 Bespoke AI 세탁기 24kg',
         manualPath: 'assets/data/Samsung_SEW_1.pdf',
         imagePath: 'assets/images/3G106S.png',
       ),
       WasherModel(
-        washerCode: 'Samsung 3G106M',
+        washerCode: 'Samsung_3G106M',
         washerName: '삼성 Bespoke AI 세탁기 21kg',
         manualPath: 'assets/data/Samsung_SEW_1.pdf',
         imagePath: 'assets/images/3G106M.png',
       ),
       WasherModel(
-        washerCode: 'Samsung 3G127HS',
+        washerCode: 'Samsung_3G127HS',
         washerName: '삼성 Bespoke AI 세탁기 25kg(올인원컨트롤)',
         manualPath: 'assets/data/Samsung_SEW_1.pdf',
         imagePath: 'assets/images/3G127HS.png',
       ),
       WasherModel(
-        washerCode: 'Samsung 3G107H',
+        washerCode: 'Samsung_3G107H',
         washerName: '삼성 Bespoke AI 세탁기 24kg(올인원컨트롤)',
         manualPath: 'assets/data/Samsung_SEW_1.pdf',
         imagePath: 'assets/images/3G107H.png',
       ),
       WasherModel(
-        washerCode: 'Samsung 3G136J',
+        washerCode: 'Samsung_3G136J',
         washerName: '삼성 Bespoke AI 세탁기 21kg(올인원컨트롤)',
         manualPath: 'assets/data/Samsung_SEW_1.pdf',
         imagePath: 'assets/images/3G136J.png',
@@ -72,37 +72,37 @@ class WasherModel {
 
       // SEW 2 설명서 공통
       WasherModel(
-        washerCode: 'Samsung 3G104F',
+        washerCode: 'Samsung_3G104F',
         washerName: '삼성 AI 세탁기 25kg',
         manualPath: 'assets/data/Samsung_SEW_2.pdf',
         imagePath: 'assets/images/3G104F.png',
       ),
       WasherModel(
-        washerCode: 'Samsung 3G104P',
+        washerCode: 'Samsung_3G104P',
         washerName: '삼성 AI 세탁기 21kg',
         manualPath: 'assets/data/Samsung_SEW_2.pdf',
         imagePath: 'assets/images/3G104P.png',
       ),
       WasherModel(
-        washerCode: 'Samsung MF100',
+        washerCode: 'Samsung_MF100',
         washerName: '삼성 AI 통버블 세탁기 19kg',
         manualPath: 'assets/data/Samsung_SEW_2.pdf',
         imagePath: 'assets/images/MF100.png',
       ),
       WasherModel(
-        washerCode: 'Samsung MG100',
+        washerCode: 'Samsung_MG100',
         washerName: '삼성 AI 통버블 세탁기 16kg',
         manualPath: 'assets/data/Samsung_SEW_2.pdf',
         imagePath: 'assets/images/MG100.png',
       ),
       WasherModel(
-        washerCode: 'Samsung HS100',
+        washerCode: 'Samsung_HS100',
         washerName: '삼성 통버블 세탁기 19kg',
         manualPath: 'assets/data/Samsung_SEW_2.pdf',
         imagePath: 'assets/images/HS100.png',
       ),
       WasherModel(
-        washerCode: 'Samsung HS100J',
+        washerCode: 'Samsung_HS100J',
         washerName: '삼성 통버블 세탁기 16kg',
         manualPath: 'assets/data/Samsung_SEW_2.pdf',
         imagePath: 'assets/images/HS100J.png',
